4ea1942efac2b85f1245eb62f5a5311a4c3e58c0
Paweł Polakiewicz <pp.polakiewicz@gmail.com>
2023-12-08 16:56:52 +0100

    Change to proper test container single container instance

--- backend/src/test/java/com/str/shootingresulttracker/core/AbstractIntegrationTest.java
+++ backend/src/test/java/com/str/shootingresulttracker/core/AbstractIntegrationTest.java
@@ -4,15 +4,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-@Testcontainers
 @SpringBootTest
 public abstract class AbstractIntegrationTest extends AbstractUnitTest {
 
     private static final DockerImageName POSTGRES_DOCKER_IMAGE = DockerImageName.parse("postgres:15.1-alpine3.17");
     private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(POSTGRES_DOCKER_IMAGE);
+
+    static {
+        postgres.start();
+    }
 
     @DynamicPropertySource
     static void registerPostgresProperties(DynamicPropertyRegistry registry) {
