443f05e285a44966b7b846aa6e839fe2298133ef
Paweł Polakiewicz <pp.polakiewicz@gmail.com>
2023-12-09 23:48:13 +0100

    Add missing weapon in memory repository methods

--- backend/src/test/java/com/str/shootingresulttracker/domain/magazine/WeaponInMemoryRepository.java
+++ backend/src/test/java/com/str/shootingresulttracker/domain/magazine/WeaponInMemoryRepository.java
@@ -3,12 +3,37 @@ package com.str.shootingresulttracker.domain.magazine;
 import com.str.shootingresulttracker.core.AbstractInMemoryRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public class WeaponInMemoryRepository extends AbstractInMemoryRepository<Weapon> implements WeaponRepository {
 
     @Override
     public List<WeaponDto> queryMagazineWeapons(UUID magazineId, UUID userId) {
-        return null;
+        return collection.values().stream()
+                .filter(weapon -> weapon.getMagazine().getId().equals(magazineId))
+                .map(this::toDto)
+                .toList();
+    }
+
+    @Override
+    public Optional<String> queryWeaponName(UUID weaponId, UUID ownerId) {
+        return collection.values().stream()
+                .filter(weapon -> weapon.getId().equals(weaponId))
+                .findFirst()
+                .map(Weapon::getName);
+    }
+
+    private WeaponDto toDto(Weapon weapon) {
+        return new WeaponDto(
+                weapon.getId(),
+                weapon.getName(),
+                weapon.getType(),
+                weapon.getCaliber(),
+                weapon.getModel(),
+                weapon.getProductionDate(),
+                weapon.getPurchaseDate(),
+                weapon.getCreationDate()
+        );
     }
 }
