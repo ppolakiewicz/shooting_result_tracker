af3b3af509cbe1f7f15cf5d63ff6a7aa8eb04a4c
Paweł Polakiewicz <pp.polakiewicz@gmail.com>
2023-12-21 21:15:46 +0100

    Remove file storage logic, maybe will be reverted in future

--- backend/src/main/java/com/str/shootingresulttracker/web/filestorage/FileStorageException.java
+++
@@ -1,7 +0,0 @@
-package com.str.shootingresulttracker.web.filestorage;
-
-public class FileStorageException extends RuntimeException {
-    public FileStorageException(Throwable cause) {
-        super(cause);
-    }
-}
